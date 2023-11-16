76862a888020af094baaebf26078a56b2bf4c5be
Halabaluso <dmartindiaz3@gmail.com>
2023-11-16 10:29:51 +0100

    Try something for deploy

--- app.vue
+++ app.vue
@@ -1,42 +1,11 @@
 <template>
-  <div class="drawer">
-    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content">
-      <!-- Page content here -->
-      <NuxtLayout>
-        <Loading/>
-        <HeaderComponent />
-        <HeaderWelcome />
-        <NuxtLoadingIndicator />
-        <NuxtPage />
-      </NuxtLayout>
-    </div>
-    <div class="drawer-side">
-      <MenuComponent />
-    </div>
-  </div>
+  <p>Esto es una prueba :D</p>
 </template>
 
 <script setup>
-import HeaderComponent from "./components/Header&Menu/HeaderComponent.vue"
-import MenuComponent from "./components/Header&Menu/MenuComponent.vue"
-import HeaderWelcome from "./components/Header&Menu/WelcomeHead.vue"
-import Loading from "./components/uxui/OverlayComponent.vue"
-import { LoadingStore } from "./src/store/LoadingStore"
-const loading = LoadingStore()
-let colorMode = useColorMode();
-colorMode.preference = "bumblebee"
+
 </script>
 
 <style>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
-}
 
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
-}
 </style>
--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app'
 export default defineNuxtPlugin(async nuxtApp => {
     const config = useRuntimeConfig()
-    console.log(config.public.firebaseConfig)
     initializeApp(config.public.firebaseConfig)
 })
